bbbf313077733a0c07dfe3724cb4879fab4523ec
glennkok1501 <kok.glenn@yahoo.com.sg>
2025-03-13 09:12:56 +0800

    base

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return ( 
+        <div className="container">
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+     );
+}
+ 
+export default HomePage;
